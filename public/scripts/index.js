66b5ca7d8878b60a487d1986392625f2777727a1
symphony <jciurc@gmail.com>
2022-08-02 15:21:04 -0700

    create announce and notify events. create connect/disconnect events in client

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,43 @@
 $(() => {
+  let socket = null;
+
+  // = events =
+  $('body > header > #connect').on('click', () => {
+    if (socket) return;
+    socket = connect();
+  });
+
+  $('body > header > #disconnect').on('click', () => {
+    if (!socket) return;
+    disconnect(socket);
+    socket = null;
+  });
+});
+
+// functions
+const connect = () => {
   const socket = io();
 
-});
+  socket.on('connect', (e) => {
+    console.log('Joined as:', socket.id);
+  });
+
+  socket.on('announce', (data) => {
+    $li = document.createElement('li');
+    $li.appendChild(document.createTextNode(data));
+    $('body > main > #announce > ul').prepend($li);
+  });
+
+  socket.on('notify', (data) => {
+    $div = document.createElement('div');
+    $div.appendChild(document.createTextNode(data));
+    $('body > main > #notify > ul').html($div);
+  });
+
+  return socket;
+};
+
+const disconnect = (socket) => {
+  console.log('Disconnected from server\n');
+  socket.disconnect();
+};
--- socketServer.js
+++ socketServer.js
@@ -4,8 +4,16 @@ export const listen = (httpServer) => {
   const server = new socketio.Server(httpServer);
 
   server.on('connection', (client) => {
-    console.log(client.id, 'user online');
+    console.log(client.id, 'connected', client.data);
+    client.on('disconnect', () => {
+      console.log(client.id, 'disconnected');
+    });
 
+    // Broadcast Message
+    server.emit('announce', client.id + ' is online');
+
+    // Client Message
+    server.to(client.id).emit('notify', client.id + ' is online');
 
   });
 
