c1696f6c7c710e66663f6bfd5070a0799592d9e4
symphony <jciurc@gmail.com>
2022-08-03 02:31:48 -0700

    sanitize chat input

--- src/socketServer.ts
+++ src/socketServer.ts
@@ -5,7 +5,7 @@ const onlineUsers = new Map();
 const r1: [RegExp, string] = [/[^0-9a-zA-Z_-]+/, ''];
 const r2: [RegExp, string] = [/n+[\s_\.\-]?[i1l|]+[\s_\.\-]?[gq]+[_\.\-\s]*[gq69]+[e3a4i\s]*[ra4s]*s?/ig, '****'];
 const r3: [RegExp, string] = [/(f|ph)?[\s_\.\-]?[a4]+.?[gq69]+[_\.\-\s]*[gq69]+[o0\s]+[t7]*s?/ig, '****'];
-const c = (s: string) => s.replace(...r1).replace(...r2).replace(...r3).trim();
+const c = (s: string) => s.replace(...r2).replace(...r3).trim();
 
 export const listen = (httpServer: Server) => {
   const server = new socketio.Server(httpServer);
@@ -16,7 +16,7 @@ export const listen = (httpServer: Server) => {
     let username: string | null = null;
 
     client.on('name', (data: User, callback) => {
-      username = c(data.username);
+      username = c(data.username.replace(...r1));
       onlineUsers.set(id, { id, username })
 
       console.log('Users:', onlineUsers);
@@ -29,7 +29,7 @@ export const listen = (httpServer: Server) => {
     });
 
     client.on('send', (body: string) => {
-      server.emit('chat', `[${username}] ${body}`)
+      server.emit('chat', `[${username}] ${c(body)}`)
     });
 
     // Client Message
