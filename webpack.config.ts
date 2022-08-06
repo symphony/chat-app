cb98f81f5127f87d38672249093fbe968299882e
symphony <jciurc@gmail.com>
2022-08-06 16:32:26 -0700

    install webpack and dependencies. set up config file

---
+++ webpack.config.ts
@@ -0,0 +1,63 @@
+import * as webpack from 'webpack';
+import 'webpack-dev-server';
+
+const clientConfig: webpack.Configuration = {
+  name: 'client',
+  target: 'web',
+  entry: 'src/public/scripts/client.js',
+  output: {
+    path: 'dist/public/scripts/client.js',
+    filename: 'index.js',
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+  },
+  "include": [
+    "./**/*.ts"
+  ]
+}
+const serverConfig: webpack.Configuration = {
+  name: 'server',
+  target: 'node',
+  entry: 'src/server.ts',
+  output: {
+    path: 'dist',
+    filename: 'server.js',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+  },
+  "include": [
+    "./**/*.ts"
+  ],
+};
+
+
+export default [
+  // clientConfig,
+  serverConfig
+];
