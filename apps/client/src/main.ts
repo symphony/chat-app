6b6b6bc9d27a5ebbde9d9ca39e5e6b66a0144bc5
symphony <jciurc@gmail.com>
2022-08-07 17:21:47 -0700

    compile scss files with bundler

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,5 +1,11 @@
 import App from './App.svelte';
 
+import './styles/index.scss';
+import * as M from 'materialize-css/dist/js/materialize'
+
+// init material plugins
+M.AutoInit()
+
 const app = new App({
 	target: document.body
 });
