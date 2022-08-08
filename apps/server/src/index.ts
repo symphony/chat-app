f675dc7fab999208a7644e5206f8b952973e9632
symphony <jciurc@gmail.com>
2022-08-07 17:28:21 -0700

    set up 404 and home route in server api

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,6 +8,19 @@ const app = express();
 
 app.use(express.static('dist/public'));
 
+
+app.get('/404', (req, res) => {
+  res.status(404).send('Page Not Found')
+});
+
+app.get('/*', (req, res) => {
+  res.redirect('/404')
+});
+
+app.get('/', (req, res) => {
+  res.send('Server Running')
+});
+
 // start http server
 const server = app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
