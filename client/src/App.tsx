2190f1713d291985c21f00c190f10bfb2b62d30e
symphony <jciurc@gmail.com>
2022-08-28 20:14:26 -0700

    set up material component. install react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React from "react";
+import {
+  Box,
+  CssBaseline,
+  Paper,
+  Typography,
+  ThemeProvider,
+  createTheme
+} from "@mui/material";
 
-function App() {
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#63b8ff",
+      main: "#0989e3",
+      dark: "#005db0",
+      contrastText: "#000",
+    },
+    secondary: {
+      main: "#4db6ac",
+      light: "#82e9de",
+      dark: "#00867d",
+      contrastText: "#000",
+    },
+  },
+});
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      hello
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Paper
+          elevation={3}
+          sx={{ padding: "1rem", backgroundColor: "secondary.dark" }}
+        >
+          <Typography color="primary.dark" variant="h1">
+            Chat App
+          </Typography>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 }
 
