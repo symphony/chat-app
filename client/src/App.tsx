48e34026f0c5cf13b93fd0fdbaa9a7c5fbcfb1ba
symphony <jciurc@gmail.com>
2022-08-28 17:32:55 -0700

    add jest dependency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import 'styles/App.css';
 
+import Button from '@mui/material/Button';
+
 const App = () => {
   return (
     <div className='App'>
-      <h2>hello ❄️Snowpack❄️</h2>
+      <p>
+        <header>Hello world</header>
+        <Button variant="contained" color="primary">
+          Click
+        </Button>
+      </p>
     </div>
   );
 }
