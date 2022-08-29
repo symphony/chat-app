05fc6caa236890a8448fa048ff35375d3ac9e834
symphony <jciurc@gmail.com>
2022-08-28 18:51:48 -0700

    remove broken templates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,16 @@ import './App.css';
 
 interface AppProps {}
 
-function App({}: AppProps) {
+const App = ({}: AppProps) => {
   // Create the count state.
   const [count, setCount] = useState(0);
+
   // Create the counter (+1 every second).
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
+
   // Return the App component.
   return (
     <div className="App">
