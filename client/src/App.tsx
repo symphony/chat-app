d4a11f449c27885932e0f65b5da1212d1a1ab597
symphony <jciurc@gmail.com>
2022-08-28 18:10:35 -0700

    install material packages. downgrade react 18 > 17

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'styles/App.css';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='App'>
-
+      {/* <Navbar /> */}
       <h2>hello ❄️Snowpack❄️</h2>
     </div>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 
-const container = document.getElementById('app');
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// React 18 setup
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
+// import './styles/index.css';
+
+// const container = document.getElementById('app');
+// const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+// root.render(<App />);
